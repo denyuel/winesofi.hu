7ee7ffbde222de196c65918a1970440b19b8cd4c
Hanna Uri <101406038+Uhanna7@users.noreply.github.com>
2024-03-14 14:45:50 +0100

    padding fix

--- src/pages/kapcsolat.js
+++ src/pages/kapcsolat.js
@@ -10,7 +10,7 @@ export default function Contact() {
           <StaticImage className='w-full' src='../images/narrow-hero-bg.png'/>
         </div>
         <div className='flex flex-col'>
-          <div className='flex flex-col md:flex-row justify-center items-center gap-4 md:gap-12 px-4 md:px-20 xl:px-[30rem] py-12'>
+          <div className='flex flex-col md:flex-row justify-center items-center gap-2 px-4 md:px-20 xl:px-72 py-12'>
             <div className='flex flex-col items-center md:items-start gap-8 w-11/12 md:w-8/12'>
               <h1 className='text-3xl md:text-4xl title'>kapcsolat</h1>
               <div class="border-t-2 border-gray w-10"></div>
@@ -18,9 +18,9 @@ export default function Contact() {
                 Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies.
               </p>
             </div>
-            <div className='flex justify-center h-64 w-11/12 md:w-4/12 -mb-20'>
-              <div className='w-64 h-full overflow-hidden z-10 rounded-full'>
-                <StaticImage src='../images/contact.jpg'/>
+            <div className='flex justify-end w-11/12 md:w-4/12 -mb-20'>
+              <div className='w-64 h-64 overflow-hidden z-10 rounded-full aspect-square'>
+                <StaticImage className='rounded-full' src='../images/contact.jpg'/>
               </div>
             </div>
           </div>
