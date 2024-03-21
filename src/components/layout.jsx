fcb2b3764284026d4beb6649a8fd2c1dc0d33bb0
Hanna Uri <101406038+Uhanna7@users.noreply.github.com>
2024-03-21 09:08:02 +0100

    minor fix

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -6,18 +6,18 @@ const Layout = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col">
       <header className="bg-white py-4 px-2">
-        <div className="container mx-auto xl:px-4">
+        <div className="container mx-auto lg:px-4">
           <Header />
         </div>
       </header>
       <main className="flex-grow">
-        <div className="mx-auto xl:px-4">
+        <div className="mx-auto lg:px-4">
           {/* Main content here */}
           {children}
         </div>
       </main>
       <footer className="bg-gray_light py-4 px-2">
-        <div className="container mx-auto xl:px-4">
+        <div className="container mx-auto lg:px-4">
           <Footer />
         </div>
       </footer>
