459089449d8e4cfc203146a7186c62c6be7dd697
Hanna Uri <101406038+Uhanna7@users.noreply.github.com>
2024-03-22 08:42:56 +0100

    Header and footer (#12)
    * header and footer basic
    
    * Fixing navigation dots and gaps
    
    * header logo basic
    
    * minor fixes
    
    * logo container z-index
    
    * a responsive header (very rudimentary)
    
    * minor corrections
    
    * minor corrections 2.0
    
    * header icon + responsive footer
    
    * Convert css to tailwind classes in header
    
    * Fixing mobile nav list-items' positions
    
    * md, lg screen header + footer
    
    * Fixing view break points
    
    * header-footer md, lg screen fix
    
    * .
    
    * Fixing mobile nav
    
    * Fix logo position
    
    * responsive header, footer fix
    
    * minor fix
    
    * header logo minor fix
    
    * hero image global class
    
    * navbar fix for different screen sizes
    
    * header navbar fix
    
    * minor fix
    
    ---------
    
    Co-authored-by: Marcellokah <aalma47@gmail.com>
    Co-authored-by: Peter Almasi <Peter_Almasi@outlook.com>

---
+++ src/components/footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import LeftNav from './left_nav';
+import RightNav from './right_nav';
+import { StaticImage } from "gatsby-plugin-image";
+import logoSvg from '../images/logo.svg';
+import fb from "../images/facebook.svg";
+import insta from "../images/instagram.svg";
+
+const Footer = () => {
+  return (
+    <div className='flex gap-8'>
+        <div className='flex w-2/3 md:w-1/6 justify-start gap-3 xl:gap-4'>
+            <div className='flex max-w-[4.25rem] max-h-[3.5rem]'>
+                <img src={logoSvg} alt="logo"/>
+            </div>
+            <div className='flex items-center w-40'>
+                <span className='nav-text'>Copyright © 2023</span>
+            </div>
+            </div>
+        <div className='hidden md:flex w-10/12'>
+            <div className="w-7/12">
+                <LeftNav />
+            </div>
+            <div className="flex items-center justify-center w-4 m-2 xl:m-4">
+                <StaticImage src="../images/ellipse.svg"/>
+            </div>
+            <div className="w-5/12">
+                <RightNav />
+            </div>
+        </div>
+        <div className="flex items-center justify-end gap-2 md:hidden w-1/3">
+          <a href="https://www.facebook.com/" className="hover:text-gray-300">
+            <img src={fb} alt="facebook" className="w-6 h-6 mx-2" />
+          </a>
+          <a href="https://www.instagram.com/" className="hover:text-gray-300">
+            <img src={insta} alt="instagram" className="w-7 h-7 mx-2" />
+          </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
