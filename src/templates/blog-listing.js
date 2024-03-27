8e6fd3579007204c1336ba90d734520f44443cc5
Hanna Uri <101406038+Uhanna7@users.noreply.github.com>
2024-03-27 13:11:56 +0100

    blog-listing page

--- src/templates/blog-listing.js
+++ src/templates/blog-listing.js
@@ -8,26 +8,8 @@ export default function Blog({ data }) {
   const nodes = data.allSanityPost.nodes;
   return (
     <Layout>
-      <div>
-        <h1 className="text-2xl font-bold">Blog</h1>
-
-        {nodes.map(node => (
-          <div className="pb-8" key={node.id}>
-            <Link to={`/post/${node.slug.current}`}>
-              <h2 className="text-xl font-bold">{node.title}</h2>
-            </Link>
-            <div className="prose max-w-none">
-              <PortableText
-                value={node._rawSummary}
-              // components={/* optional object of custom components to use */}
-              />
-            </div>
-
-            <GatsbyImage image={node.mainImage.asset.gatsbyImageData} alt={node.title} />
-          </div>
-        ))}
-
-      </div>
+        <div className='narrow-hero mb-16'></div>
+        <BlogList nodes={nodes} />
     </Layout>
   )
 }
