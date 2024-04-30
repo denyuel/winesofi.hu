b6631eb48e24f0bdd51aad28ffcaf9922beec649
Peter Almasi <Peter_Almasi@outlook.com>
2024-04-30 09:02:46 +0200

    contact page content

--- src/pages/kapcsolat.js
+++ src/pages/kapcsolat.js
@@ -10,15 +10,21 @@ export default function Contact() {
       <div>
         <div className='bg-narrow-hero w-full min-h-28 bg-cover'></div>
         <div className='flex flex-col'>
-          <div className='flex flex-col md:flex-row justify-center items-center gap-2 px-4 md:px-20 xl:px-72 py-12'>
+          <div className='container mx-auto flex flex-col md:flex-row justify-center items-center gap-2 px-4 py-12'>
             <div className='flex flex-col items-center md:items-start gap-8 w-11/12 md:w-8/12'>
               <h1 className='text-3xl md:text-4xl title'>kapcsolat</h1>
               <div className="border-t-2 border-gray w-10"></div>
               <p className='bold-text text-center md:text-left'>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies.
+                Lépj velünk kapcsolatba elérhetőségeink egyikén, vagy küldd el az alábbi űrlapot és hamarosan felvesszük Veled a kapcsolatot.
+              </p>
+              <p className='py-8'>
+                <b className='font-bold'>Wine&Sofi borászat<br /><br />Laposa Zsófia</b><br />
+                8261 Badacsonytomaj, Római út 197/1.<br />
+                +36 20 505 1607<br />
+                zsofialaposa@gmail.com<br />
               </p>
             </div>
-            <div className='flex justify-center md:justify-end w-11/12 md:w-4/12 -mb-20'>
+            <div className='flex justify-center md:justify-end w-11/12 md:w-4/12'>
               <div className='w-64 h-64 overflow-hidden z-10 rounded-full aspect-square'>
                 <StaticImage className='rounded-full' src='../images/contact.jpg' alt='' />
               </div>
