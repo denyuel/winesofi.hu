ce433de0a481c8218edd5432ade09e41e220070a
Peter Almasi <Peter_Almasi@outlook.com>
2024-04-30 11:20:10 +0200

    age modal update

--- src/components/ageVerificationModal.jsx
+++ src/components/ageVerificationModal.jsx
@@ -21,9 +21,9 @@ const AgeVerificationModal = () => {
   if (!displayModal) return null;
 
   return (
-    <div className='fixed flex justify-center items-center top-0 left-0 z-50 bg-white bg-opacity-70 w-screen h-screen'>
+    <div className='fixed flex justify-center items-center top-0 left-0 z-50 bg-blue bg-opacity-60 w-screen h-screen'>
       <div className='m-6 w-full mx-auto max-w-screen-sm sm:top-14 bg-white shadow-xl rounded-md border-slate-400 p-8 text-center'>
-        <h1 className='text-lg pb-6 font-bold'>ELMÚLT MÁR 18 ÉVES?</h1>
+        <div className='text-lg pb-6 font-bold'>ELMÚLT MÁR 18 ÉVES?</div>
         <p className='pb-8'>
           A honlap megtekintéséhez legalább 18 évesnek kell lennie.
         </p>
