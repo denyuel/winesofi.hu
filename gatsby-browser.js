640946a4d51fbf6136bef7e79e4406664f4fab59
Peter Almasi <Peter_Almasi@outlook.com>
2024-05-07 08:08:11 +0200

    cookie policy & privacy policy

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,12 +3,14 @@ import './src/styles/global.css'
 import '@fontsource/cormorant-sc/700.css';
 import '@fontsource/kaushan-script';
 import AgeVerificationModal from './src/components/ageVerificationModal';
+import CookiePolicy from './src/components/cookiePolicy';
 
 const PageWrapper = ({ children }) => {
   return (
     <div className="page-wrapper">
       {children}
       <AgeVerificationModal />
+      <CookiePolicy />
     </div>
   )
 };
@@ -18,4 +20,23 @@ export const wrapPageElement = ({ element }) => {
     {element}
   </PageWrapper>
   );
+};
+
+export function onRouteUpdate({ location }) {
+  if (
+    process.env.NODE_ENV === `production`
+  ) {
+    const pagePath = location
+      ? location.pathname + location.search + location.hash
+      : undefined
+    setTimeout(() => {
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({
+        event: 'pageview',
+        page: {
+          path: pagePath
+        }
+      });
+    }, 50)
+  }
 };
