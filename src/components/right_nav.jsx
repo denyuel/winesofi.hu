69a1ae2457a872aa67aac088fb144a532b0ecc35
Peter Almasi <nemvalid@users.noreply.github.com>
2024-05-14 13:46:34 +0200

    Update right_nav.jsx

--- src/components/right_nav.jsx
+++ src/components/right_nav.jsx
@@ -8,7 +8,7 @@ const RightNav = () => {
     <nav className="container md:mx-auto flex justify-between items-center h-full uppercase">
       <div className="flex flex-col md:flex-row flex-1 justify-between items-center">
         <div className="flex flex-col md:flex-row flex-1 items-center justify-start xl:space-x-4 gap-3 xl:gap-4 nav-text">
-          <Link to="/boraink" className="hover:text-gray-300">Borbutik</Link>
+          <Link to="/borok" className="hover:text-gray-300">Borbutik</Link>
           <span className="hidden md:flex items-center text-lg leading-none text-slate-200">&bull;</span>
           <Link to="/blog" className="hover:text-gray-300">
             Blog
